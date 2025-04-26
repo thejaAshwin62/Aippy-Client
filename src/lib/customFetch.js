731963d845a51d9df1f2924e91ef9a06960c5f49
thejaAshwin62 <thejaashwin62@gmail.com>
2025-04-27 01:20:52 +0530

    v1

--- src/lib/customFetch.js
+++ src/lib/customFetch.js
@@ -2,7 +2,24 @@ import axios from "axios";
 
 const customFetch = axios.create({
     baseURL: 'https://aipply-springboot.onrender.com/api/v1',
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
 })
+
+// Add request interceptor to handle CORS
+customFetch.interceptors.request.use(
+    (config) => {
+        // Add CORS headers to every request
+        config.headers['Access-Control-Allow-Origin'] = 'https://aipply-silk.vercel.app';
+        config.headers['Access-Control-Allow-Credentials'] = 'true';
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default customFetch;
