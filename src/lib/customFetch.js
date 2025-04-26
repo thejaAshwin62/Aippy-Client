4b35f873d177eca9693043526befcb1d08d06cac
thejaAshwin62 <thejaashwin62@gmail.com>
2025-04-27 01:19:29 +0530

    v1

--- src/lib/customFetch.js
+++ src/lib/customFetch.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-    baseURL:'/api/v1',
+    baseURL: 'https://aipply-springboot.onrender.com/api/v1',
+    withCredentials: true
 })
+
 export default customFetch;
